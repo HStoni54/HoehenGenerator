569d0d459714c6c6ce33799d60acadb7ec361472
HStoni54 <50242025+HStoni54@users.noreply.github.com>
2019-10-18 15:51:52 +0200

    Ich beginne die Hgt-Filebestimmung zu modularisieren

---
+++ HoehenGenerator/HgtmitKoordinaten.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HoehenGenerator
+{
+    class HgtmitKoordinaten
+    {
+        string name;
+       int dezLat;
+        int dezLon;
+
+        public HgtmitKoordinaten(string name, int dezLat, int dezLon)
+        {
+            this.name = name;
+            this.dezLat = dezLat;
+            this.dezLon = dezLon;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int DezLat { get => dezLat; set => dezLat = value; }
+        public int DezLon { get => dezLon; set => dezLon = value; }
+    }
+}
---
+++ HoehenGenerator/VierEcken.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HoehenGenerator
+{
+    class VierEcken
+    {
+        GeoPunkt linksunten;
+        GeoPunkt rechtsoben;
+        HgtmitKoordinaten hgtlinksunten;
+        HgtmitKoordinaten hgtlinksoben;
+        HgtmitKoordinaten hgtrechtsunten;
+        HgtmitKoordinaten hgtrechtsoben;
+
+        public VierEcken(GeoPunkt linksunten, GeoPunkt rechtsoben)
+        {
+            this.linksunten = linksunten;
+            this.rechtsoben = rechtsoben;
+            BestimmePunkte();
+        }
+
+        private void BestimmePunkte()
+        {
+            hgtlinksoben = BestimmeHgtFile( linksunten.Lon, rechtsoben.Lat,"lo");
+            hgtlinksunten = BestimmeHgtFile(linksunten.Lon, linksunten.Lat, "lu");
+            hgtrechtsoben = BestimmeHgtFile(rechtsoben.Lon, rechtsoben.Lat, "ro");
+            hgtrechtsunten = BestimmeHgtFile(rechtsoben.Lon, linksunten.Lat, "ru");
+        }
+
+        private HgtmitKoordinaten BestimmeHgtFile(double lon, double lat, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal HgtmitKoordinaten Hgtlinksunten { get => hgtlinksunten; set => hgtlinksunten = value; }
+        internal HgtmitKoordinaten Hgtlinksoben { get => hgtlinksoben; set => hgtlinksoben = value; }
+        internal HgtmitKoordinaten Hgtrechtsunten { get => hgtrechtsunten; set => hgtrechtsunten = value; }
+        internal HgtmitKoordinaten Hgtrechtsoben { get => hgtrechtsoben; set => hgtrechtsoben = value; }
+    }
+}
