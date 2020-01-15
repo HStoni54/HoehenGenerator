80ca828b764112ec07e0cb1323ac5f295495ca35
HStoni54 <50242025+HStoni54@users.noreply.github.com>
2020-01-15 20:41:12 +0100

    Beginne Berechnungen OSM_Koordinaten

--- HoehenGenerator/OSM_Koordinaten.cs
+++ HoehenGenerator/OSM_Koordinaten.cs
@@ -10,8 +10,9 @@ namespace HoehenGenerator
     {
         private GeoPunkt geoPunkt;
         private int osmauflösung = 0;
+        private int kachelanzahl;
         private int osmbreite;
-        private int osmhöhe;
+        private int osmlänge;
         private int kachelhöhe;
         private int kachelbreite;
 
@@ -22,23 +23,28 @@ namespace HoehenGenerator
 
         public OSM_Koordinaten(GeoPunkt geoPunkt, int osmauflösung) : this(geoPunkt)
         {
-            this.Osmauflösung = osmauflösung;
+            this.osmauflösung = osmauflösung;
+            kachelanzahl = (int)Math.Pow(2, this.osmauflösung);
         }
 
         public int Osmbreite { get => osmbreite; set => osmbreite = value; }
-        public int Osmhöhe { get => osmhöhe; set => osmhöhe = value; }
+        public int Osmlänge { get => osmlänge; set => osmlänge = value; }
         public int Osmauflösung { get => osmauflösung; set => osmauflösung = value; }
         public int Kachelhöhe { get => kachelhöhe; set => kachelhöhe = value; }
         public int Kachelbreite { get => kachelbreite; set => kachelbreite = value; }
 
         public void BerechneOSMKachel(int osmauflösung)
         {
-            this.Osmauflösung = osmauflösung;
+            this.osmauflösung = osmauflösung;
+            kachelanzahl = (int)Math.Pow(2, this.osmauflösung);
             BerechneOSMKachel();
         }
 
         public void BerechneOSMKachel()
         {
+            osmbreite = (int)(90 - geoPunkt.Lat)/180*kachelanzahl;
+            osmlänge = (int)(180 - geoPunkt.Lon)/360*kachelanzahl;
+
 
         }
     }
