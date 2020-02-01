db12493f5c25da539cc6b8b7afecdb93626c2b9f
HStoni54 <50242025+HStoni54@users.noreply.github.com>
2020-02-01 17:33:02 +0100

    OSM-Kachelberechnung berichtigt

--- HoehenGenerator/OSM_Koordinaten.cs
+++ HoehenGenerator/OSM_Koordinaten.cs
@@ -16,6 +16,8 @@ namespace HoehenGenerator
         private int osmlänge;
         private int kachelhöhe;
         private int kachelbreite;
+        private double kachell;
+        private double kachelb;
         private string dateiname;
 
         public OSM_Koordinaten(GeoPunkt geoPunkt)
@@ -34,6 +36,9 @@ namespace HoehenGenerator
         public int Osmauflösung { get => osmauflösung; set => osmauflösung = value; }
         public int Kachelhöhe { get => kachelhöhe; set => kachelhöhe = value; }
         public int Kachelbreite { get => kachelbreite; set => kachelbreite = value; }
+        public double Kachell { get => kachell; set => kachell = value; }
+        public double Kachelb { get => kachelb; set => kachelb = value; }
+
         public void BerechneOSMKachel(GeoPunkt geoPunkt, int osmauflösung)
         {
             this.geoPunkt = geoPunkt;
@@ -48,22 +53,30 @@ namespace HoehenGenerator
 
         public void BerechneOSMKachel()
         {
-            osmbreite = (int)((((90 - geoPunkt.Lat ) )/ 180 * kachelanzahl) ); // TODO Breite-Werte stimmen nicht 85.0511 arctan(sinh(pi))
-    //    Reproject the coordinates to the Mercator projection (from EPSG:4326 to EPSG:3857):
-    //        x = lon
-    //    y = arsinh(tan(lat)) = log[tan(lat) + sec(lat)]
+            //osmbreite = (int)((((90 - geoPunkt.Lat ) )/ 180 * kachelanzahl) ); // TODO Breite-Werte stimmen nicht 85.0511 arctan(sinh(pi))
+            //    Reproject the coordinates to the Mercator projection (from EPSG:4326 to EPSG:3857):
+            //        x = lon
+            //    y = arsinh(tan(lat)) = log[tan(lat) + sec(lat)]
 
-    //    (lat and lon are in radians)
+            //    (lat and lon are in radians)
 
-    //Transform range of x and y to 0 – 1 and shift origin to top left corner:
-    //        x = [1 + (x / π)] / 2
-    //    y = [1 − (y / π)] / 2
-    //Calculate the number of tiles across the map, n, using 2zoom
-    //Multiply x and y by n.Round results down to give tilex and tiley.
-
-            osmlänge = (int)((((180 + geoPunkt.Lon ) ) / 360 * kachelanzahl) );
-            kachelbreite = (int)(((90 - geoPunkt.Lat) / 180 * kachelanzahl * 512) % 512);
-            kachelhöhe = (int)(((180 + geoPunkt.Lon) / 360 * kachelanzahl * 512) % 512);
+            //Transform range of x and y to 0 – 1 and shift origin to top left corner:
+            //        x = [1 + (x / π)] / 2
+            //    y = [1 − (y / π)] / 2
+            //Calculate the number of tiles across the map, n, using 2zoom
+            //Multiply x and y by n.Round results down to give tilex and tiley.
+            //double y = Math.Log(Math.Tan(geoPunkt.Lat * Math.PI / 180) + 1 / Math.Cos(geoPunkt.Lat * Math.PI / 180));
+            //y = (1 - (y / Math.PI)) / 2;
+            double osmb = (1 - (Math.Log(Math.Tan(geoPunkt.Lat * Math.PI / 180) + 1/Math.Cos(geoPunkt.Lat * Math.PI / 180)))/Math.PI)/2 * kachelanzahl;
+            double osml = (180 + geoPunkt.Lon) / 360 * kachelanzahl;
+            osmlänge = (int)osml;
+            osmbreite = (int)osmb;
+            Kachelb = osmb - osmbreite;
+            Kachell = osml - osmlänge;
+            //kachelbreite = (int)(((90 - geoPunkt.Lat) / 180 * kachelanzahl * 512) % 512);
+            //kachelhöhe = (int)(((180 + geoPunkt.Lon) / 360 * kachelanzahl * 512) % 512);
+            kachelbreite = (int)(Kachell * 512);
+            kachelhöhe = (int)(Kachelb * 512);
             dateiname = osmauflösung.ToString(CultureInfo.CurrentCulture) + "_" + osmbreite.ToString(CultureInfo.CurrentCulture) + "_" + osmlänge.ToString(CultureInfo.CurrentCulture); // TODO IFormatprovider einsetzen
         }
     }
