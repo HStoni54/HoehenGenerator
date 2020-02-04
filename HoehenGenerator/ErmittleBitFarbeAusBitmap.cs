8968b18d45531ca9734d7d029e7d7073ad0818a4
HStoni54 <50242025+HStoni54@users.noreply.github.com>
2020-02-04 11:31:13 +0100

    Umwandlung png nach bmp

--- HoehenGenerator/ErmittleBitFarbeAusBitmap.cs
+++ HoehenGenerator/ErmittleBitFarbeAusBitmap.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -12,25 +13,37 @@ namespace HoehenGenerator
     {
         Color pixelColor;
         String bilddateiname;
+        String neuerbildname;
         Bitmap ausgangsbild;
         Bitmap bearbeitungsbild;
+        Rectangle rechteck;
         double osmhöhe;
         double osmbreite;
         int bildbreite;
         int bildhöhe;
+        PixelFormat pixelFormat = PixelFormat.Format24bppRgb;
 
         public ErmittleBitFarbeAusBitmap(string bilddateiname, double osmhöhe, double osmbreite)
         {
             this.bilddateiname = bilddateiname;
             this.osmhöhe = osmhöhe;
             this.osmbreite = osmbreite;
-            if (this.bilddateiname.EndsWith(".png",true, CultureInfo.CurrentCulture))
+            if (this.bilddateiname.EndsWith(".png", true, CultureInfo.CurrentCulture))
                 WandleBildUm();
+            else if (this.bilddateiname.EndsWith(".bmp", true, CultureInfo.CurrentCulture))
+                neuerbildname = bilddateiname;
         }
 
         private void WandleBildUm()
         {
-            throw new NotImplementedException();
+            ausgangsbild = new Bitmap(bilddateiname);
+            bildbreite = ausgangsbild.Width;
+            bildhöhe = ausgangsbild.Height;
+            rechteck = new Rectangle(0, 0, bildbreite, bildhöhe);
+            bearbeitungsbild = ausgangsbild.Clone(rechteck, pixelFormat);
+            neuerbildname = bilddateiname.Substring(0, bilddateiname.Length - 4) + ".bmp";
+            bearbeitungsbild.Save(neuerbildname, ImageFormat.Bmp);
+
         }
 
         public Color PixelColor { get => pixelColor; set => pixelColor = value; }
