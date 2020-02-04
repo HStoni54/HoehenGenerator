2e6b5e59f7870aa73b557422a5654ae0ea41cd6b
HStoni54 <50242025+HStoni54@users.noreply.github.com>
2020-02-04 11:01:09 +0100

    Beginne Farbermittlung

---
+++ HoehenGenerator/ErmittleBitFarbeAusBitmap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoehenGenerator
+{
+    class ErmittleBitFarbeAusBitmap
+    {
+        Color pixelColor;
+        String bilddateiname;
+        Bitmap ausgangsbild;
+        Bitmap bearbeitungsbild;
+        double osmhöhe;
+        double osmbreite;
+        int bildbreite;
+        int bildhöhe;
+
+        public ErmittleBitFarbeAusBitmap(string bilddateiname, double osmhöhe, double osmbreite)
+        {
+            this.bilddateiname = bilddateiname;
+            this.osmhöhe = osmhöhe;
+            this.osmbreite = osmbreite;
+            if (this.bilddateiname.EndsWith(".png",true, CultureInfo.CurrentCulture))
+                WandleBildUm();
+        }
+
+        private void WandleBildUm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Color PixelColor { get => pixelColor; set => pixelColor = value; }
+    }
+}
